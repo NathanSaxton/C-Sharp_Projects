5a4b50e31d2e38e2c97a1dcb4f01488a17c724aa
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-26 14:48:29 -0600

    Created a folder for basic learning assignments.
    Added my first assignment to the folder.

---
+++ Basic_C#_Projects/myConsoleProject.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you");
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Welcome, " + name + "!");
+    }
+}
+
