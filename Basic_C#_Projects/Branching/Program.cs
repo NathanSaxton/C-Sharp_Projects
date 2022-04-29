f3a7eb1787999768fdea592ef070ee214e5c3500
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-29 14:27:51 -0600

    Created the Console_App_Submission
    Created a program to show while and do while loops and that they can successfully be broken.

--- Basic_C#_Projects/Branching/Program.cs
+++ Basic_C#_Projects/Branching/Program.cs
@@ -7,9 +7,9 @@ class Program
     {
         Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nPlease provide package weight.");
         short weight = Convert.ToInt16(Console.ReadLine());
-        if (weight > 50)
+        if (weight > 50)//determine if the package is too heavy
         {
-            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");//prompt before ending the program early
             Console.ReadLine();
         } else
         {
@@ -19,16 +19,16 @@ class Program
             short height = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Length:");
             short length = Convert.ToInt16(Console.ReadLine());
-            int dimensions = width + height + length;
+            int dimensions = width + height + length;//get the dimensions total
             
-            if (dimensions > 50)
+            if (dimensions > 50)//determine if the dimensions total is more than 50
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Package too big to be shipped via Package Express.");//prompt before ending the program early
                 Console.ReadLine();
             } else
             {
                 int quote = (width * height * length * weight)/100;
-                Console.WriteLine("Your estimated total for shipping this package is $" + quote.ToString() + ".00\nThank you!");
+                Console.WriteLine("Your estimated total for shipping this package is $" + quote.ToString() + ".00\nThank you!");//message for providing quote before ending the program
                 Console.ReadLine();
             }
         }
---
+++ Basic_C#_Projects/Console_App_Submission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number.");
+        short number = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Turn " + number.ToString() +" into 10");
+        if (number < 10) { //this will initiate the while loop
+            while (number < 10) //set the condition
+            {
+                Console.WriteLine("Keep Growing!");//show that it's iterating through each time. 
+                number++;//add a way to meet that condition
+            }
+            Console.WriteLine("The number you entered is now " + number.ToString());
+        } else //this will initiate the do while loop
+        {
+            do
+            {
+                Console.WriteLine("It's too big it needs to shrink more!");//iterate through this
+                number--;//while making changes
+            } while (number > 10);//that will cause the number to meet this condition. 
+            Console.WriteLine("The number you entered is now " + number.ToString());
+        }
+        Console.ReadLine();
+    }
+}
+
