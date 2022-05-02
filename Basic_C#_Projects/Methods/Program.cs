e794ec4a373a9c11d366747b5cd1c8a0272efeb4
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-02 15:10:54 -0600

    Created the Method's Project

---
+++ Basic_C#_Projects/Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("User please enter a number to get some QuickMath done.");
+            try
+            {
+                int entry = Convert.ToInt32(Console.ReadLine());
+                QuickMath quickMath = new QuickMath();//instantiate the QuickMath class and assign it a variable name.
+
+                int results = quickMath.Add(entry);//call the add method
+                Console.WriteLine("Adding your number by itself equals: " + results);
+
+                results = quickMath.Multiply(entry);//call the multiply method
+                Console.WriteLine("Multiplying your number by itself equals: " + results);
+
+                results = quickMath.Plus10(entry);//call the plus10 method
+                Console.WriteLine("Your number plus 10 equals: " + results);
+            }
+            catch (Exception ex)//just incase
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally//Complete the program
+            {
+                Console.WriteLine("Thank you, come again.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Basic_C#_Projects/Methods/QuickMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class QuickMath
+    {
+        public int Add(int x)
+        {
+            int y = x + x;
+
+            return y;
+        }
+        public int Multiply(int x)
+        {
+            int y = x * x;
+
+            return y;
+        }
+        public int Plus10(int x)
+        {
+            int y = x + 10;
+            
+            return y;
+        }
+    }
+}
