d68dc8eef839d416dcef375200516651dc7a3a2f
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-02 11:13:07 -0600

    Created Strings_And_Integers
    Created a program demonstrating try/catch usage.

---
+++ Basic_C#_Projects/Strings_And_Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>() { 33, 23, 54, 32, 60, 74, 48 };
+        Console.WriteLine("Please enter a number to divide the numbers in my list.");
+        try //user entry following the loop through the list. 
+        {
+            int entry = Convert.ToInt32(Console.ReadLine());//get user input
+            foreach (int number in numbers)//divide each integer in numbers by the user's entry
+            {
+                int result = number / entry;
+                Console.WriteLine(number + " divided by " + entry + " equals " + result);
+            }
+        }
+        catch (Exception ex)//generic exception errow with the error message following
+        {
+            Console.WriteLine("Something went wrong.\n" + ex.Message);
+            return;
+        }
+        finally //run this no matter what
+        {
+            Console.WriteLine("We have successfully emerged from the try/catch block.");
+            Console.ReadLine();
+        }
+    }
+}
+
