8575462ef43034b07229dcab1af26708c66e3fb7
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-03 00:52:56 -0600

    Created the Main_Method project

---
+++ Basic_C#_Projects/Main_Method_Submission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Main_Method_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            QuickAdd quick = new QuickAdd();//instantiate an instance of the QuickAdd Class
+            int number = quick.Math(4);//use the first method
+            Console.WriteLine("Here is a quick number: " + number);
+
+            int results = quick.Math(48.2821m);//overloaded method 1
+            Console.WriteLine("Here is another quick number: " + results);
+
+            int anotherNumber = quick.Math("8");//overloaded method 2
+            Console.WriteLine("Here is your last quick number: " + anotherNumber);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Basic_C#_Projects/Main_Method_Submission/QuickAdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Method_Submission
+{
+    public class QuickAdd
+    {
+        public int Math(int x)//initial method
+        {
+            int y = x + 11;
+
+            return y;
+        }
+
+        public int Math(decimal x)//first overload
+        {
+            int y = Convert.ToInt32(x) - 5;
+
+            return y;
+        }
+        public int Math(string x)//second overload
+        {
+            int y = Convert.ToInt32(x);
+            y /= 2;
+
+            return y;
+        }
+    }
+}
