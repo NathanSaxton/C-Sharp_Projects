c84f90bd4b60fa782d34a70702e322e142fbf6f9
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-04 16:47:16 -0600

    Created Class_Submission

---
+++ Basic_C#_Projects/Class_Submission/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Submission
+{
+    public class Class1
+    {
+        public void Divide( int x)//void method used to divide by 2
+        {
+            x /= 2;
+            Console.WriteLine("Your number divided by 2 equals: " + x);//Since we are not returning anything the results should be displayed before the method ends
+        }
+        public static int Divide()//created a method without parameters, overloaded it, and set it as static
+        {
+            int result = 10 / 2;
+
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Projects/Class_Submission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Class_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 classius = new Class1();
+
+            Console.WriteLine("Please enter a number.");
+            int entry = Convert.ToInt32(Console.ReadLine());//get user's entry to divide by 2
+
+            classius.Divide(entry);//pass the entry through the method that will divide by 2 and display the results
+
+
+            int myNumber = Class1.Divide();//used the overloaded static method without any parameters
+            Console.WriteLine("Here is another one that I did... " + myNumber);//displayed the results
+
+            Console.ReadLine();
+        }
+    }
+}
