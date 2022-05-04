15a4d2dbee67d822a02d465d89a40b4e4713574d
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-04 12:59:52 -0600

    Created Main_Method_Submission project

---
+++ Basic_C#_Projects/Methods_Optional_Parameters/AnotherClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_Optional_Parameters
+{
+    public class AnotherClass
+    {
+        public int AnotherDollar(int x, int y = 1)
+        {
+            int z = x * y;
+            return z;
+        }
+    }
+}
---
+++ Basic_C#_Projects/Methods_Optional_Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods_Optional_Parameters
+{
+    class Program
+    {
+        static void Main()
+        {
+            AnotherClass ssdd = new AnotherClass();
+            Console.WriteLine("Please enter your first number:");
+            int entry1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may enter a second number but it is completely optional.");
+            int entry2;
+            int result;
+
+            if (int.TryParse(Console.ReadLine(), out entry2))
+            {
+                result = ssdd.AnotherDollar(entry1, entry2);
+            } else
+            {
+                result = ssdd.AnotherDollar(entry1);
+            }
+            Console.WriteLine("Here is the result: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
