24093df4c6dfe25dbbcbc4132b1218c06c8cb09d
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-04 13:06:06 -0600

    Update Program.cs
    added comments to the program.

--- Basic_C#_Projects/Methods_Optional_Parameters/Program.cs
+++ Basic_C#_Projects/Methods_Optional_Parameters/Program.cs
@@ -8,19 +8,19 @@ namespace Methods_Optional_Parameters
         {
             AnotherClass ssdd = new AnotherClass();
             Console.WriteLine("Please enter your first number:");
-            int entry1 = Convert.ToInt32(Console.ReadLine());
+            int entry1 = Convert.ToInt32(Console.ReadLine());//get first input
             Console.WriteLine("You may enter a second number but it is completely optional.");
             int entry2;
             int result;
 
-            if (int.TryParse(Console.ReadLine(), out entry2))
+            if (int.TryParse(Console.ReadLine(), out entry2))//had to use TryParse to make the input optional. 
             {
-                result = ssdd.AnotherDollar(entry1, entry2);
+                result = ssdd.AnotherDollar(entry1, entry2);//using the optional parameter
             } else
             {
-                result = ssdd.AnotherDollar(entry1);
+                result = ssdd.AnotherDollar(entry1);//without using the optional parameter
             }
-            Console.WriteLine("Here is the result: " + result);
+            Console.WriteLine("Here is the result: " + result);//display results
 
             Console.ReadLine();
         }
