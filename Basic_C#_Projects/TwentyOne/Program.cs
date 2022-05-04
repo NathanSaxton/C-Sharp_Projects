711d878bd18c58f938f56952a3436ad4f31f66c7
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-04 16:23:52 -0600

    Moved the shuffle method to the Deck Class

--- Basic_C#_Projects/TwentyOne/Program.cs
+++ Basic_C#_Projects/TwentyOne/Program.cs
@@ -8,36 +8,16 @@ namespace TwentyOne
         static void Main()
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck, out int timesShuffled, 4);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine("There are a total of " + deck.Cards.Count + " cards in this deck.");
-            Console.WriteLine("The deck was shuffled: " + timesShuffled + " times");
             Console.ReadLine();
         }
         
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    TempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = TempList;
-            }
-
-            return deck;
-        }
+       
     }
 }
