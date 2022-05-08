697ae4602036e4abdfa709f2431b0f2fe4b487d8
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-08 13:00:34 -0600

    Created the Struct Project

---
+++ Basic_C#_Projects/Structs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number()//instantiate the Number struct
+            {
+                Amount = 516.1648m//assign a decimal to the amount
+            };
+
+            Console.WriteLine(number.Amount);//print the amount
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic_C#_Projects/Structs/Struct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs
+{
+    public struct Number//struct called number
+    {
+        public decimal Amount { get; set; }//property called amount
+    }
+}
