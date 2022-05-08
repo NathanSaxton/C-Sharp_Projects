1e0d2330189bbd7b45932a8a6298b10d1d97b841
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-08 12:02:10 -0600

    Created Parsing_Enums
    Created and completed my first submission for Parsing_Enums

---
+++ Basic_C#_Projects/Parsing_Enums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                DaysOfTheWeek today = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);//take the user input and parse it to the Enum DaysOfTheWeek also works with number input
+
+                Console.WriteLine(today);//write the day of the week that the user entered
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\nPlease enter an actual day of the week");//write the exception message followed by the requested phrase
+            }
+
+            Console.ReadLine();
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
