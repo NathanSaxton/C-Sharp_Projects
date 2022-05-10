78e9194f2bb462b4d3f9edf678ed4bfd3779edfc
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-10 15:32:59 -0600

    Created the Constructor_Submission project

---
+++ Basic_C#_Projects/Constructor_Submission/Pirate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor_Submission
+{
+    class Pirate
+    {
+        public string Name { get; set; }
+        public int KillCount { get; set; }
+
+        public Pirate(string name, int kills)
+        {
+            Name = name;
+            KillCount = kills;
+        }
+        public Pirate(string name) : this (name, 0)
+        {
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/Constructor_Submission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string pirateName = "Beakbeard";//set a const string
+            
+            var pirate1 = new Pirate(pirateName);//used const string to set a var using a chained constructor
+
+            Console.WriteLine("{0} has {1} kills.", pirate1.Name, pirate1.KillCount);
+            Console.ReadLine();
+        }
+    }
+}
