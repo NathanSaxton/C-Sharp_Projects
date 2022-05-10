3092a22ec55e1767ccd2bffcac338329d3b980d6
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-10 11:42:00 -0600

    Created DateTime_Submission project

---
+++ Basic_C#_Projects/DateTime_Submission/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateTime_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today + "\nGive a number for the amount of hours we want to skip.");
+            double userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime later = today.AddHours(userInput);
+            Console.WriteLine("The new time is:\n" + later);
+        }
+    }
+}
