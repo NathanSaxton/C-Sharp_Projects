c1ead7be7d1c8bea348403d611d05526647b2ad9
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-12 14:38:43 -0600

    Created the NewsLetterMVC project

---
+++ Basic_C#_Projects/NewsLetterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                var signUps = db.SignUps.Where(x => x.Removed == null).ToList();
+                var signUpsVMs = new List<SignUpsVM>();
+                foreach (var signUp in signUps)
+                {
+                    var signUpVM = new SignUpsVM();
+                    signUpVM.Id = signUp.id;
+                    signUpVM.FirstName = signUp.FirstName;
+                    signUpVM.LastName = signUp.LastName;
+                    signUpVM.EmailAddress = signUp.EmailAddress;
+                    signUpsVMs.Add(signUpVM);
+                }
+
+                return View(signUpsVMs);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Basic_C#_Projects/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterEntities1 db = new NewsletterEntities1())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                    return View("Success");
+            }
+        }
+            }
+}
