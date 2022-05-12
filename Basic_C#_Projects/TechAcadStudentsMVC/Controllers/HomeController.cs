24f5c995f8bfbc8897eb20239e6a3e58b8c74bdc
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-11 22:16:27 -0600

    Created TechAcademyStudents project

---
+++ Basic_C#_Projects/TechAcadStudentsMVC/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechAcadStudentsMVC.Models;
+
+namespace TechAcadStudentsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Page - The Tech Academy";
+
+            return View();
+        }
+
+        public ActionResult Instructor(int id)
+        {
+            ViewBag.Id = id;
+            Instructor dayTimeInstructor = new Instructor
+            {
+                Id = 1,
+                FirstName = "Nathan",
+                LastName = "Saxton"
+            };
+            return View();
+        }
+
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>()
+            {
+                new Instructor
+                {
+                    Id = 2,
+                    FirstName = "Rick",
+                    LastName = "Ross"
+                },
+                new Instructor
+                {
+                    Id = 3,
+                    FirstName = "Mariah",
+                    LastName = "Saxton",
+                },
+                new Instructor
+                {
+                    Id = 4,
+                    FirstName = "Jordan",
+                    LastName = "Spieth"
+                }
+            };
+            return View(instructors);
+        }
+    }
+}
