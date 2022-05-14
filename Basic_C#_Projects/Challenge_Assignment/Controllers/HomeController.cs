cccacbed92cf85214d9c3775560f75b877a71cbb
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-05-14 01:58:51 -0600

    Created Challenge Assignment project and published to azure

---
+++ Basic_C#_Projects/Challenge_Assignment/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Challenge_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            DateTime today = DateTime.Now;
+            return View(today);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
